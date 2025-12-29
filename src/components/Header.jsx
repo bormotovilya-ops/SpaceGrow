0dd75eb6a854e1a8ec66c9d3149f988f62b82f64
User <best_email@mail.ru>
2025-12-29 13:24:39 +0300

    Update sales funnel: increase block sizes for mobile, fill screen without empty space

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,6 +29,9 @@ function Header({ onAvatarClick, onConsultation, onBack }) {
             +7 (999) 123-77-88
           </a>
           <button className="header-consultation-btn" onClick={onConsultation}>
+            <svg className="telegram-icon" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.562 8.16l-1.87 8.81c-.14.625-.52.78-1.05.485l-2.9-2.14-1.4 1.345c-.13.13-.24.24-.49.24l.21-2.98 5.36-4.84c.23-.21-.05-.33-.36-.12l-6.62 4.17-2.85-.89c-.62-.19-.64-.62.13-.95l11.1-4.28c.51-.19.96.11.79.68z" fill="currentColor"/>
+            </svg>
             Бесплатная диагностика
           </button>
         </div>
