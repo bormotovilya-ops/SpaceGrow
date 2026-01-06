727caa12d61bcd32b45ca8af2b402597a977e6c9
User <best_email@mail.ru>
2026-01-06 23:44:37 +0300

    Improve header design: merge profile block, remove phone, rename diagnostic button. Fix decision cells in cards - always 3 rows. Update working cards formulations.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,38 +2,30 @@ import React from 'react'
 import './Header.css'
 
 function Header({ onAvatarClick, onConsultation, onBack }) {
-  const handlePhoneClick = () => {
-    window.location.href = 'tel:+79991237788'
-  }
-
   return (
     <div className="header-block">
       <div className="header-content">
-        <div className="header-avatar-wrapper">
-          {onBack && (
-            <button className="header-back-btn" onClick={onBack}>
-              ← Назад
-            </button>
-          )}
+        <button 
+          className={`header-back-btn ${onBack ? 'visible' : 'hidden'}`} 
+          onClick={onBack}
+        >
+          ← Назад
+        </button>
+        <div className="header-profile-block" onClick={onAvatarClick}>
           <img 
             src="/images/me.jpg" 
             alt="Илья Бормотов" 
             className="header-avatar" 
-            onClick={onAvatarClick}
           />
+          <div className="header-name-wrapper">
+            <div className="header-name">Бормотов Илья</div>
+            <h1 className="header-title">Архитектор АИЦП</h1>
+          </div>
         </div>
-        <div className="header-name-block" onClick={onAvatarClick}>
-          <div className="header-name">Бормотов Илья</div>
-          <h1 className="header-title">Архитектор АИЦП</h1>
-        </div>
-        <div className="header-contact-block">
-          <a href="tel:+79991237788" className="header-phone" onClick={handlePhoneClick}>
-            +7 (999) 123-77-88
-          </a>
-          <button className="header-consultation-btn" onClick={onConsultation}>
-            Диагностика воронки
-          </button>
-        </div>
+        <button className="header-consultation-btn" onClick={onConsultation}>
+          Диагностика вашей воронки
+        </button>
+        <div className="header-spacer-right"></div>
       </div>
     </div>
   )
