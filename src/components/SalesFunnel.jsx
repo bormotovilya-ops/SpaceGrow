b120b266998e4fe1a794d371dc70ad4d65a1bb16
User <best_email@mail.ru>
2026-01-10 01:17:32 +0300

    Р’РѕСЃСЃС‚Р°РЅРѕРІР»РµРЅ Р±Р»РѕРє РџСЂРѕРґСѓРєС‚ РІ РјРѕР±РёР»СЊРЅРѕР№ РІРµСЂСЃРёРё: СѓР±СЂР°РЅРѕ СЃРєСЂС‹С‚РёРµ, РѕР±РЅРѕРІР»РµРЅР° РІС‹СЃРѕС‚Р° Р±Р»РѕРєРѕРІ РґР»СЏ 6 СЌР»РµРјРµРЅС‚РѕРІ, РёСЃРїСЂР°РІР»РµРЅС‹ СЃС‚СЂРµР»РєРё

--- src/components/SalesFunnel.jsx
+++ src/components/SalesFunnel.jsx
@@ -160,7 +160,7 @@ function SalesFunnel() {
               </div>
               
               {/* Стрелка (вертикальная) */}
-              {index < 4 && (
+              {index < 5 && (
                 <div className={`funnel-arrow ${block.id === 'autofunnel' ? 'product-arrow' : ''}`}>
                   <svg width="20" height="20" viewBox="0 0 20 20" className="arrow-svg">
                     <line 
@@ -190,34 +190,6 @@ function SalesFunnel() {
               )}
             </React.Fragment>
           ))}
-          
-          {/* Стрелка к результатам */}
-          <div className="funnel-arrow">
-            <svg width="20" height="20" viewBox="0 0 20 20" className="arrow-svg">
-              <line 
-                x1="10" 
-                y1="0" 
-                x2="10" 
-                y2="15" 
-                stroke="#ffffff" 
-                strokeWidth="2"
-                strokeOpacity="0.8"
-                markerEnd="url(#arrowhead-result)"
-              />
-              <defs>
-                <marker 
-                  id="arrowhead-result" 
-                  markerWidth="10" 
-                  markerHeight="10" 
-                  refX="10" 
-                  refY="5" 
-                  orient="auto"
-                >
-                  <polygon points="0 0, 10 5, 0 10" fill="#ffffff" fillOpacity="0.8" />
-                </marker>
-              </defs>
-            </svg>
-          </div>
           
           {/* Блок результата (Деньги) */}
           <div className="result-blocks-container">
