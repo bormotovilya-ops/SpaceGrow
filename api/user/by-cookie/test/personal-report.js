08f090eb1f781f42d464a64fb2a84fa785da16ad
User <best_email@mail.ru>
2026-01-26 00:15:10 +0300

    test(api): remove temporary test endpoint

--- api/user/by-cookie/test/personal-report.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json; charset=utf-8')
-  return res.json({ ok: true, path: '/api/user/by-cookie/test/personal-report', ts: new Date().toISOString() })
-}
